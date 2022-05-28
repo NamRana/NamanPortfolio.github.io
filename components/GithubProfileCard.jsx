bc75311f226bc8007736987eb00506de55c98d0a
Error <91771208+NamRana@users.noreply.github.com>
2022-05-29 01:08:22 +0530

    Update GithubProfileCard.jsx

--- components/GithubProfileCard.jsx
+++ components/GithubProfileCard.jsx
@@ -12,12 +12,6 @@ const GithubProfileCard = ({ prof }) => {
 				<div className="p-2">
 					<Row className="">
 						<Col className="order-lg-2" lg="4">
-							<img
-								src={prof.avatar_url}
-								style={{ width: "200px" }}
-								alt=""
-								className="rounded-circle img-center img-fluid shadow shadow-lg--hover mb-4"
-							/>
 						</Col>
 						<Col lg="8" className="order-lg-1">
 							<h2 className="text-white">Reach Out to me!</h2>
@@ -25,11 +19,7 @@ const GithubProfileCard = ({ prof }) => {
 								DISCUSS A PROJECT OR JUST WANT TO SAY HI? MY
 								INBOX IS OPEN FOR ALL
 							</p>
-							<p className="text-white mt-3">{prof.bio}</p>
-							<div className="my-3 icon-shape bg-gradient-white shadow rounded text-info">
-								<i className="ni ni-pin-3 text-info mr-2" />
-								{prof.location}
-							</div>
+							
 							<SocialLinks />
 						</Col>
 					</Row>
